d5076e4fe6af2287812d2052dd87ec53ac78243c
OgTushar <145109237+TusharAmbast@users.noreply.github.com>
2026-03-26 12:02:31 +0530

    Modified config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
+// next.config.mjs or next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', 
+      },
+    ],
+  },
 };
 
 export default nextConfig;
