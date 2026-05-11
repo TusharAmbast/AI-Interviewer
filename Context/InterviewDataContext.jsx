096ac941073ab9afe9f1dc1068a766176fa162e3
OgTushar <145109237+TusharAmbast@users.noreply.github.com>
2026-05-11 13:20:10 +0530

    Interview Data Context Stored

---
+++ Context/InterviewDataContext.jsx
@@ -0,0 +1,14 @@
+"use client"
+import { createContext, useState } from "react";
+
+export const InterviewDataContext = createContext();
+
+export const InterviewDataProvider = ({ children }) => {
+  const [interviewInfo, setInterviewInfo] = useState(null);
+  
+  return (
+    <InterviewDataContext.Provider value={{ interviewInfo, setInterviewInfo }}>
+      {children}
+    </InterviewDataContext.Provider>
+  );
+};
