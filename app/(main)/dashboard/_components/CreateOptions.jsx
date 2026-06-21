5c02ef32a16e411cdc2d02021a2e91881b4a7cde
OgTushar <145109237+TusharAmbast@users.noreply.github.com>
2026-06-21 10:12:50 +0530

    Enabled dark mode functionality

--- app/(main)/dashboard/_components/CreateOptions.jsx
+++ app/(main)/dashboard/_components/CreateOptions.jsx
@@ -10,15 +10,15 @@ function CreateOptions() {
     const route = useRouter();
     return (
         <div className='grid grid-cols-2 gap-5'>
-            <div onClick={() => route.push('/dashboard/create-interview')} className='bg-white border border-gray-200 rounded-lg p-5 cursor-pointer hover:shadow-md transition-shadow duration-300'>
-                <Video className='p-3 text-primary bg-gray-100 rounded-lg h-12 w-12' />
-                <h2 className='font-bold text-xl'>Create New Interview</h2>
-                <p className='text-gray-500'>Start a new AI-driven interview session</p>
+            <div onClick={() => route.push('/dashboard/create-interview')} className='bg-card border border-border rounded-lg p-5 cursor-pointer hover:shadow-md transition-shadow duration-300'>
+                <Video className='p-3 text-primary bg-primary/10 rounded-lg h-12 w-12' />
+                <h2 className='font-bold text-xl text-card-foreground mt-2'>Create New Interview</h2>
+                <p className='text-muted-foreground'>Start a new AI-driven interview session</p>
             </div>
-            <div className='bg-white border border-gray-200 rounded-lg p-5 cursor-pointer hover:shadow-md transition-shadow duration-300' onClick={() => route.push('/all-interview')}>
-                <GalleryThumbnails className='p-3 text-primary bg-gray-100 rounded-lg h-12 w-12' />
-                <h2 className='font-bold text-xl'>View Interviews</h2>
-                <p className='text-gray-500'>Review and manage your scheduled interviews</p>
+            <div className='bg-card border border-border rounded-lg p-5 cursor-pointer hover:shadow-md transition-shadow duration-300' onClick={() => route.push('/all-interview')}>
+                <GalleryThumbnails className='p-3 text-primary bg-primary/10 rounded-lg h-12 w-12' />
+                <h2 className='font-bold text-xl text-card-foreground mt-2'>View Interviews</h2>
+                <p className='text-muted-foreground'>Review and manage your scheduled interviews</p>
 
             </div>
         </div>
